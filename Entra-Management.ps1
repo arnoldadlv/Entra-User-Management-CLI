f5ebca2e11cbf37c14bc4fc2d69a6e67169dfb30
Arnold De La Vega <arnoldd@device-0004.local>
2024-12-14 14:22:39 -0800

    Added user options for connecting to specific environments like GCC High or USGov DoD.

--- Entra-Management.ps1
+++ Entra-Management.ps1
@@ -1,3 +1,94 @@
+<# try {
+    Connect-MgGraph -ErrorAction Stop
+    
+}
+catch {
+    Write-Error "Unable to connect to Microsoft Graph. Please make sure you are signed in first."
+}
+    #>
+
+function Show-EnvironmentMenu {
+    Write-Host "+----------------------------------+" -ForegroundColor Cyan
+    Write-Host "|       Select Environment         |" -ForegroundColor Cyan
+    Write-Host "+----------------------------------+" -ForegroundColor Cyan
+    Write-Host "| 1. Commercial (Default)          |"
+    Write-Host "| 2. GCC                           |"
+    Write-Host "| 3. GCC High                      |"
+    Write-Host "| 4. DoD                           |"
+    Write-Host "| 5. Exit                          |"
+    Write-Host "+----------------------------------+" -ForegroundColor Cyan
+}
+
+function Get-EnvironmentChoice {
+    Write-Host
+    $envChoice = Read-Host "Enter your choice (1-5)"
+    return $envChoice
+}
+
+function Connect-ToEnvironment {
+    $connected = $false
+    do {
+        Show-EnvironmentMenu
+        $envChoice = Get-EnvironmentChoice
+
+        switch ($envChoice) {
+            1 {
+                Write-Host "Connecting to Commercial Environment..." -ForegroundColor Yellow
+                try {
+                    Connect-MgGraph -ErrorAction Stop
+                    Write-Host "Connected to Commercial Environment." -ForegroundColor Green
+                    $connected = $true
+                }
+                catch {
+                    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+                }
+            }
+            2 {
+                Write-Host "Connecting to GCC Environment" -ForegroundColor Yellow
+                try {
+                    Connect-MgGraph -Environment USGov -ErrorAction Stop
+                    Write-Host "Connected to GCC Enviornment" -ForegroundColor Green
+                    $connected = $true
+                }
+                catch {
+                    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+                }
+            }
+            3 {
+                Write-Host "Connecting to GCC High Environment" -ForegroundColor Yellow
+                try {
+                    Connect-MgGraph -Environment USGov -ErrorAction Stop
+                    Write-Host "Connected to GCC High Environment" -ForegroundColor Green
+                    $connected = $true
+                }
+                catch {
+                    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+                }
+            }
+            4 {
+                Write-Host "Connecting to US DoD Environment" -ForegroundColor Yellow
+                try {
+                    Connect-MgGraph -Environment USGovDoD -ErrorAction Stop
+                    Write-Host "Connected to US DoD Environment" -ForegroundColor Green
+                    $connected = $true
+                }
+                catch {
+                    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+                }
+            }
+            5 {
+                Write-Host "Exiting..." -ForegroundColor Cyan
+                exit
+            }
+            default {
+                Write-Host "Invalid choice. Please select a valid option." -ForegroundColor Red
+            }
+        }
+    } while (-not $connected) {
+        
+    }
+}
+Connect-ToEnvironment
 function Show-MainMenu {
     # Clear-Host
     Write-Host "+----------------------------------+" -ForegroundColor Cyan
@@ -50,6 +141,32 @@ function Show-FindaUserMenu {
 
 }
 
+function Show-UpdateAUserMenu {
+    Write-Host "+----------------------------------+" -ForegroundColor Cyan
+    Write-Host "|         Update a User            |"
+    Write-Host "+----------------------------------+" -ForegroundColor Cyan
+    Write-Host "| 1. Update Name                   |"
+    Write-host "| 2. Update Email                  |"
+    Write-host "| 3.                               |"
+    Write-host "| 4.                               |"
+    Write-host "| 5. Return to Main Menu           |"
+    Write-host "+----------------------------------+" -ForegroundColor Cyan
+    Write-Host "DEBUG: Entering Update a User Menu"
+
+
+}
+
+function Update-UsersFirstName {
+    $usersPrincipalName = Read-Host "What user do you want to update? Enter their User Principal Name: "
+    $desiredFirstName = Read-Host "Enter the name you wish to replace the user with: "
+    
+    Update-MgUser -UserId $usersPrincipalName -GivenName $desiredFirstName
+    Write-Host "Updating user info..."
+
+    Get-MgUser $usersPrincipalName 
+
+}
+
 #Runs the Search by Name option
 function Search-UserByName {
     $userSearchTerm = Read-Host "Enter the users first name: "
@@ -68,7 +185,7 @@ function Search-UserByName {
         }
     }
     catch {
-        <#Do this if a terminating exception happens#>
+        <#Do this if a  exception happens#>
         Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
     }
 
@@ -92,6 +209,7 @@ do {
         }
         2 {
             Write-Host "Option 2: Update a User" -ForegroundColor Yellow
+            Show-UpdateAUserMenu
         }
         3 {
             Write-Host "Option 3: Create a New User" -ForegroundColor Yellow
